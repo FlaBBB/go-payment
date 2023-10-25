1a43dea8ab86377b633243da32b242016e0f9afc
FlaBBB <f12345ff67@gmail.com>
2023-10-25 10:51:48 +0700

    fix: change to gin support

--- server/helper.go
+++ server/helper.go
@@ -1,12 +1,16 @@
-package server
+package gateway
 
 import (
-	"encoding/json"
 	"errors"
-	"net/http"
-
 	"github.com/FlaBBB/go-payment"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+type Error struct {
+	StatusCode int    `json:"error_code"`
+	Message    string `json:"error_message"`
+}
 
 type Meta struct {
 	TotalItems  int    `json:"total_items"`
@@ -15,39 +19,19 @@ type Meta struct {
 	Cursor      string `json:"last_cursor"`
 }
 
-// Error is struct used to return error message to the client
-type Error struct {
-	StatusCode int    `json:"error_code"`
-	Message    string `json:"error_message"`
-}
-
 // Empty used to return nothing
 type Empty struct{}
 
-// WriteSuccessResponse creates success response for the http handler
-func WriteSuccessResponse(w http.ResponseWriter, statusCode int, data interface{}, headMap map[string]string) {
-	w.Header().Add("Content-Type", "application/json")
+func WriteSuccessResponse(c *gin.Context, statusCode int, data interface{}, headMap map[string]string) {
 	if headMap != nil && len(headMap) > 0 {
 		for key, val := range headMap {
-			w.Header().Add(key, val)
+			c.Header(key, val)
 		}
 	}
-	w.WriteHeader(statusCode)
-	jsonData, _ := json.Marshal(data)
-	w.Write(jsonData)
+	c.JSON(statusCode, data)
 }
 
-// WriteFailResponse creates error response for the http handler
-func WriteFailResponse(w http.ResponseWriter, statusCode int, error interface{}) {
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(statusCode)
-	jsonData, _ := json.Marshal(error)
-	w.Write(jsonData)
-}
-
-// WriteFailResponseFromError creates error response based on the given error
-func WriteFailResponseFromError(w http.ResponseWriter, err error) {
-
+func WriteFailResponseFromError(c *gin.Context, err error) {
 	var statusCode int
 	if errors.Is(err, payment.ErrNotFound) {
 		statusCode = http.StatusNotFound
@@ -72,8 +56,10 @@ func WriteFailResponseFromError(w http.ResponseWriter, err error) {
 		StatusCode: statusCode,
 	}
 
-	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(errorMsg.StatusCode)
-	jsonData, _ := json.Marshal(errorMsg)
-	w.Write(jsonData)
+	c.JSON(statusCode, errorMsg)
+}
+
+// WriteFailResponse creates error response for the http handler
+func WriteFailResponse(c *gin.Context, statusCode int, error interface{}) {
+	c.JSON(statusCode, error)
 }
